68d61ce832bf5926d4b8b5dd4f9f6aa68a78bc99
cellichm <103275696+cellichm@users.noreply.github.com>
2023-09-12 20:37:11 +0000

    Replace in-memory database with project model

--- src/memory_storage.js
+++ src/memory_storage.js
@@ -1,28 +1,80 @@
 // Datoteka: src/memory_storage.js
 
 let storage = {
-	posts: [
+	users: [
 		{
-			id: 10001,
-			createdBy: "nikola@tankovic.me",
-			postedAt: "1576599080230",
-			source: "https://picsum.photos/id/669/500/500",
-			title: "pula more",
+			username: 'mcellich',
+			email: 'mcellich@email.com',
+			car: {
+				type: 'BMW Series 3 2018',
+				registration: 'PU123NO',
+			},
+		},
+	],
+	washSteps: [
+		{
+			id: 1,
+			description: 'Ispiranje običnom vodom',
+			durationMinutes: 5,
+			price: 1,
 		},
 		{
-			id: 10002,
-			createdBy: "marko@markovic.me",
-			postedAt: "1577349684270",
-			source: "https://picsum.photos/id/234/500/500",
-			title: "zagreb kopno",
+			id: 2,
+			description: 'Pranje pjenom',
+			durationMinutes: 15,
+			price: 1,
 		},
 		{
-			id: 10003,
-			createdBy: "iva@ivkovic.me",
-			postedAt: "1577349684270",
-			source: "https://picsum.photos/id/12/500/500",
-			title: "rijeka luka",
+			id: 3,
+			description: 'Ispiranje demineraliziranom vodom',
+			durationMinutes: 5,
+			price: 2,
+		},
+		{
+			id: 4,
+			description: 'Vosak',
+			durationMinutes: 30,
+			price: 2,
 		}
+	],
+	washPrograms: [
+		{
+			id: 1,
+			name: 'Standardno pranje',
+			steps: [1, 2, 4],
+			durationMinutes: 50,
+		},
+		{
+			id: 2,
+			name: 'Ispiranje',
+			steps: [1],
+			durationMinutes: 5,
+		},
+		{
+			id: 2,
+			name: 'Ispiranje + vosak',
+			steps: [1, 4],
+			durationMinutes: 35,
+		}
+	],
+	locations: [
+		{
+			id: 'pula-city-mall',
+			name: 'Pula City Mall',
+			address: 'Ul. Rimske centurijacije 101, 52100, Pula',
+			workHours: '0-24',
+			contact: '+38552123456',
+		}
+	],
+	orders: [
+		{
+			location: 'pula-city-mall',
+			user: 'mcellich',
+			date: '2023-09-10',
+			timeStart: '11:00',
+			timeEnd: '11:50',
+			washProgram: 1,
+		},
 	]
 }
 export default storage;
